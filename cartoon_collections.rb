1877d50cd9fc8dc9d175320ac704457b1773be43
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-04-05 16:58:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,10 @@ def find_the_cheese(cheese_types)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find do |cheese|
+    if 
     cheese== "cheddar" || cheese== "gouda" || cheese == "camembert"
+  else 
+    nil 
   end 
   
 end
